0f399c3b42aa0fae907a921a5041084e3cafb26b
Matt Menezes <mattmenezes@mattmenezes.home>
2021-04-10 16:14:19 -0300

    First project commit.

---
+++ celsiusToFahrenheitTable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    float celsius, fahr;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("------ Celsius to fahrenheit table ------\n");
+
+    while (celsius <= upper) {
+        fahr = (celsius * 1.8) + 32;
+        printf("%3.0f %3.0f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
---
+++ fahrToCelsiusTable.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+Prints Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; floating-point version
+*/
+
+void main() {
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("------ Fahrenheit to Celsius table ------\n");
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
