782cca86f65e5cc93443986a1c80b73de81477d3
Matt Menezes <mattmenezes@mattmenezes.home>
2021-04-12 15:39:31 -0300

    Re-organized the folders into book sections (E.g. 1.1, 1.2, etc); Renamed exercise programs for easier location.

--- Chapter1/1.2/CelsiusToFahrenheitTable/Exercise_1_4.c
+++ Chapter1/1.2/CelsiusToFahrenheitTable/Exercise_1_4.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/*
-Prints Celsius-Fahrenheit table for celsius = 0, 20, ..., 300; floating-point version
-*/
 void main() {
     float celsius, fahr;
     float lower, upper, step;
@@ -12,8 +9,6 @@ void main() {
     step = 20;
 
     celsius = lower;
-
-    printf("------ Celsius to fahrenheit table ------\n");
 
     while (celsius <= upper) {
         fahr = (celsius * 1.8) + 32;
