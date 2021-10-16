9eb6890edec67c892d3f3dab106ddef8b65408e4
Matheus Menezes <mathsemilio@gmail.com>
2021-10-16 18:42:44 -0300

    Solved exercise 1-8 - Blanks, Tabs and New Lines counter

---
+++ Chapter1/1.5/BlanksTabsNewLinesCounter/BlanksTabsNewLinesCounter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int blanksCounter = 0;
+    int tabsCounter = 0;
+    int newLinesCounter = 0;
+    
+    char currentChar;
+    
+    while ((currentChar = getchar()) != EOF) {
+        if (currentChar == ' ')
+            ++blanksCounter;
+        if (currentChar == '\t') {
+            ++tabsCounter;
+        }
+        if (currentChar == '\n') {
+            ++newLinesCounter;
+        }
+    }
+    
+    printf("%d", blanksCounter);
+    printf("%d", tabsCounter);
+    printf("%d", newLinesCounter);
+}
